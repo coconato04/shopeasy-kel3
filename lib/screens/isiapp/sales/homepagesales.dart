a6897bd669254b5b62b46be6ef1e634a82339c9c
coconato04 <ardiansyahjayawinata@gmail.com>
2023-05-01 21:51:34 +0700

    tahap 3

--- lib/screens/isiapp/sales/homepagesales.dart
+++ lib/screens/isiapp/sales/homepagesales.dart
@@ -1,5 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:shopeasy/screens/isiapp/content/salesmode.dart';
 
 class HomeStore extends StatefulWidget {
   @override
@@ -7,6 +8,16 @@ class HomeStore extends StatefulWidget {
 }
 
 class _HomeStoreState extends State<HomeStore> {
+  late DatabaseReference _databaseReference;
+
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp().then((value) {
+      _databaseReference = FirebaseDatabase.instance.reference();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,302 +67,97 @@ class _HomeStoreState extends State<HomeStore> {
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            right: 10,
-                            left: 15), // Ubah padding sisi kiri di sini
-                        child: Container(
-                          width: double.infinity,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 16),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey
-                                .shade300, // Ubah warna container menjadi hitam
-                            border: Border.all(color: Colors.black, width: 2),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                StreamBuilder(
+                  stream: _databaseReference != null
+                      ? _databaseReference.child('products').onChildAdded
+                      : null,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DatabaseEvent> snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else if (!snapshot.hasData) {
+                      return Text('Loading...');
+                    } else {
+                      final data = snapshot.data!.snapshot.value as Map;
+                      final products =
+                          data.entries.map((entry) => entry.value).toList();
+                      return Column(
+                        children: products.map((product) {
+                          return Row(
                             children: [
-                              Image.asset(
-                                'assets/icon/burberry.png', // Ganti dengan path gambar yang sesuai
-                                height: 120,
-                                width: 120,
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              const Text(
-                                'Nama 1',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors
-                                      .black, // Ubah warna teks menjadi putih
-                                ),
-                              ),
-                              const Text(
-                                'Harga 1',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors
-                                      .black, // Ubah warna teks menjadi putih
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 10, left: 15),
+                                  child: Container(
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10, vertical: 16),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.grey.shade300,
+                                      border: Border.all(
+                                          color: Colors.black, width: 2),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Image.asset(
+                                          product['image'],
+                                          height: 120,
+                                          width: 120,
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          product['name'],
+                                          style: TextStyle(),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          'Rp ${product['price']}',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18),
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        ElevatedButton(
+                                          onPressed: () {
+                                            // Tambahkan aksi yang dijalankan ketika tombol 'Beli' diklik
+                                          },
+                                          child: Text('Beli'),
+                                          style: ElevatedButton.styleFrom(
+                                            primary: Colors.orange,
+                                            padding: EdgeInsets.symmetric(
+                                                horizontal: 40, vertical: 10),
+                                            textStyle: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
                                 ),
                               ),
                             ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 15, left: 5),
-                        child: Container(
-                          width: double.infinity,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 16),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey
-                                .shade300, // Ubah warna container menjadi hitam
-                            border: Border.all(color: Colors.black, width: 2),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Image.asset(
-                                'assets/icon/her loss.png', // Ganti dengan path gambar yang sesuai
-                                height: 120,
-                                width: 120,
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              const Text(
-                                'Nama 2',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors
-                                      .black, // Ubah warna teks menjadi putih
-                                ),
-                              ),
-                              const Text(
-                                'Harga 2',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors
-                                      .black, // Ubah warna teks menjadi putih
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                Row(children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          right: 10,
-                          left: 15), // Ubah padding sisi kiri di sini
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey
-                              .shade300, // Ubah warna container menjadi hitam
-                          border: Border.all(color: Colors.black, width: 2),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset(
-                              'assets/icon/hoodie cart.png', // Ganti dengan path gambar yang sesuai
-                              height: 120,
-                              width: 120,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            const Text(
-                              'Hoodie',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                            const Text(
-                              "2.99",
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 15, left: 5),
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey
-                              .shade300, // Ubah warna container menjadi hitam
-                          border: Border.all(color: Colors.black, width: 2),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset(
-                              'assets/icon/colgate.png', // Ganti dengan path gambar yang sesuai
-                              height: 120,
-                              width: 120,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            const Text(
-                              'Nama 4',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                            const Text(
-                              'Harga 4',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ]),
-                const SizedBox(height: 15),
-                Row(children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          right: 10,
-                          left: 15), // Ubah padding sisi kiri di sini
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey
-                              .shade300, // Ubah warna container menjadi hitam
-                          border: Border.all(color: Colors.black, width: 2),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset(
-                              'assets/icon/hoodie cart.png', // Ganti dengan path gambar yang sesuai
-                              height: 120,
-                              width: 120,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            const Text(
-                              'Hoodie',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                            const Text(
-                              "2.99",
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 15, left: 5),
-                      child: Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey
-                              .shade300, // Ubah warna container menjadi hitam
-                          border: Border.all(color: Colors.black, width: 2),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset(
-                              'assets/icon/colgate.png', // Ganti dengan path gambar yang sesuai
-                              height: 120,
-                              width: 120,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            const Text(
-                              'Nama 4',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                            const Text(
-                              'Harga 4',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ])
+                          );
+                        }).toList(),
+                      );
+                    }
+                  },
+                )
               ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => SalesPage()),
-          );
-        },
       ),
     );
   }
