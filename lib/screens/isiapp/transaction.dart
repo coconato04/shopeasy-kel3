d63e9d1e49c39130f07977d6b293d13431582191
coconato04 <ardiansyahjayawinata@gmail.com>
2023-05-01 08:24:19 +0700

    pembaruan transaction

--- lib/screens/isiapp/transaction.dart
+++ lib/screens/isiapp/transaction.dart
@@ -11,8 +11,14 @@ class transaction extends StatefulWidget {
 
 class _PayPageState extends State<transaction> {
   // initialize user data
-  double _balance = 1000000.0;
-
+  final double saldo = 500000.0;
+  final List<String> riwayat = [
+    "Penghasilan dari pekerjaan",
+    "Membeli bahan makanan",
+    "Penghasilan dari pekerjaan",
+    "Membayar tagihan listrik",
+    "Membeli tiket pesawat"
+  ];
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -49,23 +55,7 @@ class _PayPageState extends State<transaction> {
               child: TabBarView(
                 children: [
                   // Tabungan
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Saldo Anda",
-                          style: TextStyle(fontSize: 24.0),
-                        ),
-                        SizedBox(height: 16.0),
-                        Text(
-                          "Rp${_balance.toStringAsFixed(2)}",
-                          style: TextStyle(
-                              fontSize: 48.0, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
+                  SaldoEasypay(saldo: saldo, riwayat: riwayat),
                   // Riwayat Transaksi
                   ListView(
                     children: [
@@ -106,6 +96,78 @@ class _PayPageState extends State<transaction> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+double getProportionateScreenWidth(double inputWidth, BuildContext context) {
+  // Ganti dengan logika penghitungan lebar proporsional yang sesuai
+  // misalnya: return inputWidth * 0.8;
+  var screenWidth = MediaQuery.of(context).size.width;
+  return (inputWidth / 375.0) * screenWidth;
+}
+
+class SaldoEasypay extends StatelessWidget {
+  final double saldo;
+  final List<String> riwayat;
+
+  SaldoEasypay({required this.saldo, required this.riwayat});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+      child: Column(
+        children: [
+          Container(
+            height: getProportionateScreenWidth(100, context),
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(20, context),
+              vertical: getProportionateScreenWidth(20, context),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Saldo Easypay',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: getProportionateScreenWidth(16, context),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: getProportionateScreenWidth(10, context)),
+                Text(
+                  'Rp. ${saldo.toString()}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: getProportionateScreenWidth(20, context),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: getProportionateScreenWidth(20, context)),
+          Expanded(
+            child: ListView.builder(
+              itemCount: riwayat.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(riwayat[index]),
+                  subtitle: Text("Tanggal transaksi"),
+                  trailing: Text("+/-Jumlah transaksi"),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
